e264690af5563961a64abf823e0b7a9e10ca1628
Takasur Azeem <takasurazeem@gmail.com>
2023-10-16 04:26:17 +0500

    Initial Commit

---
+++ NotesApp Watch App/NotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesAppApp.swift
+//  NotesApp Watch App
+//
+//  Created by Takasur A. on 16/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
