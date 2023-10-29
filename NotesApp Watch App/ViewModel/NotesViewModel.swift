6aa42f7cccc0cf45dd580e30b62674c06dc097a9
Takasur Azeem <takasurazeem@gmail.com>
2023-10-29 06:19:18 +0500

    save notes to local storage

--- NotesApp Watch App/ViewModel/NotesViewModel.swift
+++ NotesApp Watch App/ViewModel/NotesViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import Combine
+import Foundation
 
 extension NotesDashboard {
     class ViewModel: ObservableObject {
@@ -14,7 +15,37 @@ extension NotesDashboard {
         
         init() {
             self.text = ""
-            self.notes.append(Note(text: "Initial Lengthy Note to see how it looks without adding a note again and again."))
+            loadNotes()
+        }
+        
+        private func save() {
+            do {
+                // Convert the notes array to data using JSONEncoder
+                let data = try JSONEncoder().encode(notes)
+                // Create a new URL to save the file using ``getDocumentDirectory`` method
+                let url = getDocumentDirectory().appendingPathComponent("notes")
+                // Write the data to given URL
+                try data.write(to: url)
+            } catch {
+                // I don't like print statements.
+                fatalError("Failed to write to file.")
+            }
+        }
+        
+        private func loadNotes() {
+            DispatchQueue.main.async { [weak self] in
+                guard let self else { return }
+                do {
+                    // Get notes url path.
+                    let url = getDocumentDirectory().appendingPathComponent("notes")
+                    // Create a new property for data.
+                    let data = try Data(contentsOf: url)
+                    // Decode data and assign notes to notes array.
+                    notes = try JSONDecoder().decode([Note].self, from: data)
+                } catch {
+                    notes = []
+                }
+            }
         }
         
         func addNote() {
@@ -22,6 +53,12 @@ extension NotesDashboard {
 //            dump(note)
             notes.append(note)
             text = ""
+            save()
+        }
+        
+        func getDocumentDirectory() -> URL {
+            let path = FileManager.default.urls(for: .documentDirectory, in: .allDomainsMask)
+            return path[0]
         }
     }
 }
